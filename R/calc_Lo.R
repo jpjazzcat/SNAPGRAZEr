abead8aaf5f098c7b4e546e0f3c4d238ee59ffbf
jpjazzcat <pennerj2@gmail.com>
2024-06-26 15:43:25 -0500

    Added DdaysO param for more explicit simulation of offseason grazing events.

--- R/calc_Lo.R
+++ R/calc_Lo.R
@@ -1,14 +1,25 @@
 #' @title Loss of biomass during the off season
 #'
 #' @description Estimating the amount of biomass lost during the dormant season (dry, non-growing season).
-#' @param Cg Daily consumption rate (g/animal/day). This is the average rate during the growing season - for the dormant season, the equations assume half this consumption rate.
-#' @param Gdays Total number of days in the growing season. Default = 153 (October to March-ish).
-#' @param d Stocking density (head/ha)
+#' @param Co Daily biomass consumption rate during the offseason (g/animal/day).
+#' @param Cg Daily biomass consumption rate during the growing season (g/animal/day).
+#' @param Gdays Total number of days in the growing season.
+#' @param DdaysO Number of days in offseason grazing episode. Default = 365 - Gdays.
+#' @param d_off Stocking density during the offseason (head/ha)
 #' @export
 
-calc_Lo= function(Cg, Gdays = 153, d_off) {
+calc_Lo= function(Cg, Co = NA, Gdays, DdaysO = NA, d_off) {
 
-  Lo = (Cg)*(365-Gdays)*d_off*10^(-4)
+  if(is.na(Co)) {
+    Co = Cg/2
+  }
+
+  if(is.na(DdaysO)) {
+    DdaysO = 365 - Gdays
+  }
+
+  Lo = (Co*DdaysO*d_off*10^(-4))
+
   return(Lo)
 
 }
